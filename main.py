d3015d41ca20134fd72402e74240e2a73131ab4f
Garegin Shakhbazyan <120056798+fyvble@users.noreply.github.com>
2022-12-09 14:57:58 +0000

    Задача 2. Дорога

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 """Задача 2. Таблица суммы
 Напишите программу, которая запрашивает у пользователя число N и выводит таблицу суммы для чисел от 1 до N."""
 
-
-
-for row in range (20):
+for row in range(20):
   for col in range(50):
-    if col  == 24:
-      print('|', end = '')
-    elif row  == 9:
-      print('-', end = '')
+    if row == 9:
+      print('-', end='')
+    elif col == 24:
+      print('|', end='')
+    elif -row + 19 == col:
+      print("/", end='')
+   
+    elif row + 29 == col:
+      print("\\", end='')
     else:
-      print(' ', end = '')
-    
+      print(' ', end='')
+
   print()
-  
